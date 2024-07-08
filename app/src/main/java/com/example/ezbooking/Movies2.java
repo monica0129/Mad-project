1eb049519ef9249460b45194e53d2c29fa60df35
Monica <monicamanimozhi20@gmail.com>
2024-07-09 01:04:49 +0530

    map

--- app/src/main/java/com/example/ezbooking/Movies2.java
+++ app/src/main/java/com/example/ezbooking/Movies2.java
@@ -3,6 +3,7 @@ package com.example.ezbooking;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Movies2 extends AppCompatActivity {
 
     View view;
+    ImageView map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +20,20 @@ public class Movies2 extends AppCompatActivity {
         setContentView(R.layout.activity_movies2);
 
         view = findViewById(R.id.view4);
+        map = findViewById(R.id.location_icon);
 
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Movies2.this, Movies3.class);
+                startActivity(intent);
+            }
+        });
+
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Movies2.this,mapping.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/ezbooking/mapping.java
@@ -0,0 +1,42 @@
+package com.example.ezbooking;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class mapping extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap gMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_mapping);
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
+        mapFragment.getMapAsync(this);
+
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        LatLng location = new LatLng(13.0843, 80.2705);
+        googleMap.addMarker(new MarkerOptions().position(location).title("TamilNadu"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,12));
+
+
+    }
+}
